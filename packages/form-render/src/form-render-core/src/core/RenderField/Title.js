cb1312f1a8573db5e95cf763fcb1fd70b91dfc59
YangsonHung <2545594421@qq.com>
2022-05-13 17:44:39 +0800

    init

---
+++ packages/form-render/src/form-render-core/src/core/RenderField/Title.js
@@ -0,0 +1,66 @@
+import { Tooltip } from 'antd';
+import React from 'react';
+import { useStore2 } from '../../hooks';
+import { isCheckBoxType } from '../../utils';
+
+const Description = ({ displayType, schema }) => {
+  const { description, descType } = schema;
+  if (!description) return null;
+
+  switch (displayType) {
+    case 'row':
+      return (
+        <Tooltip title={description}>
+          <i className="fr-tooltip-icon" />
+        </Tooltip>
+      );
+    case 'inline':
+      return null;
+    default:
+      if (descType === 'icon') {
+        return (
+          <Tooltip title={description}>
+            <i className="fr-tooltip-icon" />
+          </Tooltip>
+        );
+      }
+
+      return <span className="fr-desc ml2">{`( ${description} )`}</span>;
+  }
+};
+
+const Title = ({ labelClass, labelStyle, schema, displayType }) => {
+  const { displayType: globalDisplayType, readOnly } = useStore2();
+  const { title, required, type } = schema;
+  const isObjType = type === 'object';
+
+  let _displayType =
+    schema.displayType || displayType || globalDisplayType || 'column';
+
+  return (
+    <div className={labelClass} style={labelStyle}>
+      {title ? (
+        <label
+          className={`fr-label-title ${
+            isCheckBoxType(schema, readOnly) || _displayType === 'column'
+              ? 'no-colon'
+              : ''
+          }`} // checkbox不带冒号
+          title={title}
+        >
+          {required === true && <span className="fr-label-required"> *</span>}
+          <span
+            className={`${isObjType ? 'b' : ''} ${
+              _displayType === 'column' ? 'flex-none' : ''
+            }`}
+          >
+            <span dangerouslySetInnerHTML={{ __html: title }} />
+          </span>
+          <Description schema={schema} displayType={_displayType} />
+        </label>
+      ) : null}
+    </div>
+  );
+};
+
+export default Title;
