dc3541b46deff30335571d53918f5cbe1ed305a8
YangsonHung <2545594421@qq.com>
2022-05-19 12:24:39 +0800

    eslint格式配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,8 @@
-module.exports = require('@umijs/fabric').prettier;
+module.exports = {
+  ...require('@umijs/fabric').prettier,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  tabWidth: 4,
+  printWidth: 120,
+  jsxBracketSameLine: false,
+};
